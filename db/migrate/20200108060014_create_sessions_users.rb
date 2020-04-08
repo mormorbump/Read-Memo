000f32b8c01e29d73d65f94c14e897960ed79154
kazuki matsumoto <mormorbump@gmail.com>
2020-04-08 12:48:03 +0900

    migration整理

--- db/migrate/20200108060014_create_sessions_users.rb
+++ db/migrate/20200108060014_create_sessions_users.rb
@@ -1,14 +1,14 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-class CreateSessionsUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :sessions_users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-      t.string :image
+# class CreateSessionsUsers < ActiveRecord::Migration[6.0]
+#   def change
+#     create_table :sessions_users do |t|
+#       t.string :name
+#       t.string :email
+#       t.string :password
+#       t.string :image
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
--- db/migrate/20200108060440_create_destroys.rb
+++ db/migrate/20200108060440_create_destroys.rb
@@ -1,10 +1,10 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-class CreateDestroys < ActiveRecord::Migration[6.0]
-  def change
-    create_table :destroys do |t|
-      drop_table :sessionusers
-      t.timestamps
-    end
-  end
-end
+# class CreateDestroys < ActiveRecord::Migration[6.0]
+#   def change
+#     create_table :destroys do |t|
+#       drop_table :sessionusers
+#       t.timestamps
+#     end
+#   end
+# end
